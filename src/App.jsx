265b14b705468738244463232264768449b3e56a
ABOLFAZL FAZLABADI <abolfazlfazlabadi69@gmail.com>
2024-05-12 15:42:41 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as  BrowserRouter, Router} from 'react-router-dom'
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <p className='text-black w-full h-full'>asdfsd</p>
+      </div>
+
+      </Router>
+    </>
+  )
+}
+
+export default App
